c00d1d4bb5788a1e7c4fb44b531c584c3b738aac
Christian Severin <severinchristian25@gmail.com>
2021-04-30 08:43:12 -0400

    Initial Commit

---
+++ CapstoneProj2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CapstoneProj2.0
+//
+//  Created by Christian Severin on 4/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
